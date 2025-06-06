a3cda9ab912f3725055ef615b5105cb9d8e0165c
Oliver <oliveratgithub@users.noreply.github.com>
2025-06-06 09:12:18 +0200

    stockticker: improves pip install mode

--- resources/python/stockticker/service-setup.sh
+++ resources/python/stockticker/service-setup.sh
@@ -31,7 +31,7 @@ under certain conditions; see the LICENSE.'
 
 
 ########## Install libraries & extensions ##########
-pip install --upgrade --no-cache-dir requests schedule yfinance
+pip install --upgrade --no-cache-dir --only-binary=:all: requests schedule yfinance
 
 
 ########## Parse and run each Stock Ticker ##########
